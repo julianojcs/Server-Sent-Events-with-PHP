ab2f300c156e46fbdf0d13d72fca794036b38156
Juliano Costa Silva <apfjuliano@gmail.com>
2020-08-26 14:33:13 -0300

    First versionS

---
+++ app.js
@@ -0,0 +1,10 @@
+// cria a conexão persistente
+var evtSource = new EventSource('server.php');
+var eventList = document.querySelector('ul');
+
+evtSource.onmessage = function(e) {
+  var newElement = document.createElement("li");
+
+  newElement.textContent = "message: " + e.data;
+  eventList.appendChild(newElement);
+}
